5ff4da2157d92336f36d0b6fda994927bddeb0cc
Joseph Bayly Kumaradhas <bosebayly@gmail.com>
2018-10-10 17:50:45 -0600

    Create InvoiceBO.java

---
+++ InvoiceBO.java
@@ -0,0 +1,11 @@
+import java.util.List;
+public class InvoiceBO {
+
+	public List<Invoice> getAllInvoice() {
+		//fill the code
+	}
+	public void updateInvoiceDetails(int id,Double amount) {
+		//fill the code
+	}
+	
+}
