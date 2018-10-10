e91bc492950a19ca4c78b2ed403dc4470ca8ac1c
Joseph Bayly Kumaradhas <bosebayly@gmail.com>
2018-10-10 17:48:25 -0600

    Update and rename NewExam to CreditCardPayment.java

---
+++ CreditCardPayment.java
@@ -0,0 +1,52 @@
+
+public class CreditCardPayment   // fill the code
+{
+
+	private String cardNumber;
+	private String cvv;
+	private String cardName;
+	
+	public CreditCardPayment() {
+		
+	}
+
+	public CreditCardPayment(String name, Double amount, String cardNumber, String cvv, String cardName) {
+		super(name, amount);
+		this.cardNumber = cardNumber;
+		this.cvv = cvv;
+		this.cardName = cardName;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+
+	public String getCardName() {
+		return cardName;
+	}
+
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+
+	@Override
+	public Double calculateTotalAmount() 
+	//fill the code
+	
+	}
+	
+	
+}
+z
