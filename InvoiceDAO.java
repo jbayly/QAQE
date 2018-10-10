4d6cc6ed79c070f3ea910a6251fcf04f995d972d
Joseph Bayly Kumaradhas <bosebayly@gmail.com>
2018-10-10 17:51:21 -0600

    Create InvoiceDAO.java

---
+++ InvoiceDAO.java
@@ -0,0 +1,14 @@
+// fill the code
+
+
+
+public class InvoiceDAO {
+
+	public List<Invoice> getAllInvoices() throws ClassNotFoundException, SQLException{
+		// fill the code
+	}
+	
+	public void updateInvoiceDetails(Integer invoiceId, Double amount) throws ClassNotFoundException, SQLException{
+		// fill the code
+	}
+}
